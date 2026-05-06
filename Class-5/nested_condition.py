62b7ff1e6effc1768e9c9d545d9f084b3b723b26
tanjida-ahmed <tanjidaahmed2005@gmail.com>
2026-05-07 00:29:02 +0600

    Create nested_condition.py

---
+++ Class-5/nested_condition.py
@@ -0,0 +1,11 @@
+keyOutside = {2}
+keyInside = " "
+
+if keyOutside:
+    print("Entering main gate!!")
+    if keyInside:
+        print("Home sweet home!")
+    else:
+        print("Call you mom!")
+else:
+    print("Go to restaurant!!")
